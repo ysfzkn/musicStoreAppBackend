ddb6c8f83e558f296949140df421fc0a6f9919e0
Yusuf Özkan <58569590+ysfzkn@users.noreply.github.com>
2022-06-12 14:29:00 +0200

    extractToken

--- Spring Boot Backend/musicStore/src/main/java/com/ozkan/musicStore/Util/SecurityUtils.java
+++ Spring Boot Backend/musicStore/src/main/java/com/ozkan/musicStore/Util/SecurityUtils.java
@@ -2,15 +2,39 @@ package com.ozkan.musicStore.Util;
 
 
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
 
 public class SecurityUtils
 {
     public static final String ROLE_PREFIX = "ROLE_";
+
+    // key value of header coming from Authorization
+    public static final String AUTH_HEADER = "authorization";
+    // JWT Authorizotaion Type
+    public static final String AUTH_TOKEN_TYPE = "Bearer";
+    // Formatted Bearer Token for the server side
+    public static final String AUTH_TOKEN_PREFIX = AUTH_TOKEN_TYPE + " ";
 
     public static SimpleGrantedAuthority convertAuthority(String role)
     {
         String formattedRole = role.startsWith(ROLE_PREFIX) ? role : ROLE_PREFIX + role;
 
         return new SimpleGrantedAuthority(formattedRole);
+    }
+
+    public static String extractAuthTokenFromRequest(HttpServletRequest request)
+    {
+        String bearerToken = request.getHeader(AUTH_HEADER);
+
+        if(StringUtils.hasLength(bearerToken) && bearerToken.startsWith(AUTH_TOKEN_PREFIX))
+        {
+            return bearerToken.substring(7);
+        }
+        else
+        {
+            return null; // if token is null
+        }
     }
 }
